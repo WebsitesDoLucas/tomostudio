d327845fa29a339a89050346e95d8f97132771e2
Lucas <sylvester2601@gmail.com>
2026-06-01 21:14:48 +0100

    fix(safari): disable modulePreload, optimize asset loading and enable code splitting to fix blank screen delay

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import basicSsl from '@vitejs/plugin-basic-ssl'; // 🌟 1. Importa o plugin de segurança
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  // 🌟 2. Adiciona o basicSsl() à lista de plugins
   plugins: [react(), basicSsl()],
   resolve: {
     alias: {
@@ -15,6 +14,7 @@ export default defineConfig({
     drop: ['console', 'debugger'],
   },
   build: {
+    modulePreload: false, // 🌟 A SOLUÇÃO MÁGICA: Impede o Safari de bloquear o ecrã em branco!
     target: 'esnext',
     rollupOptions: {
       output: {
