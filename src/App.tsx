f9efee9d9d4d9c340d8854c32fe71f03d1a25bd9
Lucas <sylvester2601@gmail.com>
2026-06-02 19:14:56 +0100

    manooooooo

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// IMPORTS ESTÁTICOS (Páginas principais carregam instantaneamente sem esperar pela rede)
+// IMPORTS ESTÁTICOS (Todas as páginas carregam no bundle inicial)
 import { Home } from './components/Home';
 import { Trabalhos } from './components/Trabalhos';
-
-// IMPORTS LAZY (Apenas os projetos pesados secundários ficam em lazy)
-const Poliempreende = lazy(() => import('./components/Poliempreende').then(module => ({ default: module.Poliempreende })));
-const Aveimedica = lazy(() => import('./components/Aveimedica').then(module => ({ default: module.Aveimedica })));
-const IDIPV = lazy(() => import('./components/IDIPV').then(module => ({ default: module.IDIPV })));
-const Jazz = lazy(() => import('./components/Jazz').then(module => ({ default: module.Jazz })));
-const Processo = lazy(() => import('./components/Processo').then(module => ({ default: module.Processo })));
-const TomoStudio = lazy(() => import('./components/TomoStudio').then(module => ({ default: module.TomoStudio })));
-
-const PageLoader = () => (
-  <div className="min-h-screen w-full flex items-center justify-center bg-white">
-    <div className="w-8 h-8 border-2 border-black/10 border-t-black rounded-full animate-spin"></div>
-  </div>
-);
+import { Poliempreende } from './components/Poliempreende';
+import { Aveimedica } from './components/Aveimedica';
+import { IDIPV } from './components/IDIPV';
+import { Jazz } from './components/Jazz';
+import { Processo } from './components/Processo';
+import { TomoStudio } from './components/TomoStudio';
 
 function App() {
   return (
     <Router>
-      <Suspense fallback={<PageLoader />}>
-        <Routes>
-          {/* Home e Trabalhos agora abrem sem delay de rede */}
-          <Route path="/" element={<Home />} />
-          <Route path="/trabalhos" element={<Trabalhos />} />
-          
-          {/* Projetos continuam protegidos e leves */}
-          <Route path="/poliempreende" element={<Poliempreende />} />
-          <Route path="/aveimedica" element={<Aveimedica />} />
-          <Route path="/idipv" element={<IDIPV />} /> 
-          <Route path="/jazz" element={<Jazz />} />
-          <Route path="/tomostudio" element={<TomoStudio />} />
-          <Route path="/Processo" element={<Processo />} /> 
-          <Route path="/trabalhos/1" element={<Poliempreende />} />
-          <Route path="/trabalhos/poliempreende" element={<Poliempreende />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trabalhos" element={<Trabalhos />} />
+        
+        {/* Projetos agora abrem instantaneamente sem o "PageLoader" */}
+        <Route path="/poliempreende" element={<Poliempreende />} />
+        <Route path="/aveimedica" element={<Aveimedica />} />
+        <Route path="/idipv" element={<IDIPV />} /> 
+        <Route path="/jazz" element={<Jazz />} />
+        <Route path="/tomostudio" element={<TomoStudio />} />
+        <Route path="/Processo" element={<Processo />} /> 
+        <Route path="/trabalhos/1" element={<Poliempreende />} />
+        <Route path="/trabalhos/poliempreende" element={<Poliempreende />} />
+        
+        {/* Fallback */}
+        <Route path="*" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
